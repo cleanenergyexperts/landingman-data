c63616c2b06c73e5b5df6816f14162824f070326
Matt Snider <matt@cleanenergyexperts.com>
2016-02-16 07:12:13 -0800

    Use local constants

--- lib/landingman-data/extension.rb
+++ lib/landingman-data/extension.rb
@@ -3,6 +3,8 @@ require 'middleman-core'
 
 module Landingman
   class DataExtension < ::Middleman::Extension
+    DATA_FILE_MATCHER = /^(.*?)[\w-]+\.(yml|yaml|json)$/
+
     option :data1_dir, '../_data',  'Global Data Directory #1'
     option :data2_dir, '../data',   'Global Data Directory #2'
 
@@ -22,7 +24,7 @@ module Landingman
         if !options.data1_dir.nil? && !options.data1_dir.blank? then
           data1_dir = File.expand_path(File.join(app.root, options.data1_dir))
           if Dir.exist?(data1_dir) then
-            watcher = app.files.watch :global_data1, path: data1_dir, only: ::Middleman::CoreExtensions::Data::DATA_FILE_MATCHER
+            watcher = app.files.watch :global_data1, path: data1_dir, only: DATA_FILE_MATCHER
             app.files.on_change(:global_data1, &method(:update_data))
             watcher.poll_once!
           end
@@ -31,7 +33,7 @@ module Landingman
         if !options.data2_dir.nil? && !options.data2_dir.blank? then
           data2_dir = File.expand_path(File.join(app.root, options.data2_dir))
           if Dir.exist?(data2_dir) then
-            watcher = app.files.watch :global_data2, path: data2_dir, only: ::Middleman::CoreExtensions::Data::DATA_FILE_MATCHER
+            watcher = app.files.watch :global_data2, path: data2_dir, only: DATA_FILE_MATCHER
             app.files.on_change(:global_data2, &method(:update_data))
             watcher.poll_once!
           end
