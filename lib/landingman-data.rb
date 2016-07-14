a1706e2210f92320a4f92e25fc5e90743a0e2417
Matt Snider <matt@cleanenergyexperts.com>
2016-07-13 17:16:42 -0700

    Fixed middleman try bug

--- lib/landingman-data.rb
+++ lib/landingman-data.rb
@@ -1,5 +1,12 @@
 require "middleman-core"
 
+###
+# NOTE: There seems to be a bug in Middleman, where try is used
+# but not required, causing errors. Including it here should fix
+# this
+###
+require 'active_support/core_ext/object/try'
+
 Middleman::Extensions.register :landingman_data do
   require "landingman-data/extension"
   ::Landingman::DataExtension
